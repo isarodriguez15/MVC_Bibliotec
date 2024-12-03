64ef9ccfe51406f2ce4d3185d04dba13abfac259
ISABELLE RODRIGUES DOS REIS <46206573877@info.local>
2024-12-03 11:51:37 -0300

    ^^

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -40,6 +40,36 @@ namespace Bibliotec_mvc.Controllers
             return View();
         }
 
+        [Route("Cadastro")]
+        // metodo que retorna a tela de cadastro:
+        public IActionResult Cadastro(){
+
+            ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
+
+            ViewBag.Categorias = context.Categoria.ToList();
+            //Retorna a View de cadastro:
+             return View();
+        }
+
+        // metodo para cadastrar um livro:
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form){
+            Livro novolivro = new Livro();
+            // O que meu usuario escrever no formulario sera atribuido ao novoLivro
+            novolivro.Nome = form["Nome"].ToString();
+            novolivro.Descricao = form["Descricao"].ToString();
+            novolivro.Editora = form["Editora"].ToString();
+            novolivro.Escritor = form["Escritor"].ToString();
+            novolivro.Idioma = form["Idioma"].ToString();
+
+            // img
+            context.Livro.Add(novolivro);
+
+            context.SaveChanges();
+            
+            return View();
+        }
+
         // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         // public IActionResult Error()
         // {
